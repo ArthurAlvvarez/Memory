502a5fb132f312d246c297f2c952b0a817482b2d
ArthurAlvvarez <arturo.alvarez.gomez@colegiolitterator.com>
2024-10-25 16:22:04 +0200

    se rellena también el array tablero_jugador

--- Engine.py
+++ Engine.py
@@ -2,7 +2,8 @@ import random
 
 emojis = {1:"😀",2:"🥰",3:"😨",4:"🥶",5:"🦍",6:"👾",7:"🐲",
         8:"😡",9:"🦘",10:"🐳",11:"🐼",12:"🐺",13:"🦃",14:"🐫",15:"😎"}
-tablero = [[],[]]
+tablero_original = []
+tablero_jugador = []
 
 # Metodo que recibe por parametro el numero de filas y de columnas, si excede las 6 filas y 5 columnas o viceversa devuelve false.
 # Devuelve tambien false si el tamaño no es par o en menor que 2x2.
@@ -17,22 +18,38 @@ def max(fila,columna):
     else:
         return False
 
+#Crea dos tableros "vacios" segun las filas y columnas que haya pedido el usuario.
 def tablero_vacio(fila,columna):
     if max(fila,columna) == True:
-        for i in range(len(fila)):
-            for j in range(len(columna)):
-                tablero[[]].append(0)
+        for i in range(fila):
+            rows = []
+            for j in range(columna):
+                rows.append("+")
+            tablero_jugador.append(rows)
+            tablero_original.append(rows)
+        return True
+    else:
+        return False
 
 
 
-def crear_tablero(fila,columna):
-    if max(fila,columna) == True:
-        for i in range((fila*columna)/2):
-            numero_aleatorio = random.randint(1,len(emojis))
-            for j in range(2):
-                f1 = random.randint(0,fila-1)
-                c1 = random.randint(0,columna-1)
-                if tablero[f1][c1] != 0:
-                    tablero[f1][c1] = emojis.get(numero_aleatorio)
+def llenar_tablero(fila,columna):
+    if tablero_vacio(fila,columna) is True:
+        for i in range(int((fila*columna)/2)):
+            n = int(random.randint(1,len(emojis)))
+            emoji_aleatorio = emojis.get(n)
+            for n in range(2):
+                while True:
+                    fila_aleatoria = random.randint(0,fila-1)
+                    columna_aleatoria = random.randint(0,columna-1)
+                    if tablero_original[fila_aleatoria][columna_aleatoria] != '+':
+                        fila_aleatoria = random.randint(0,fila-1)
+                        columna_aleatoria = random.randint(0,columna-1)
+                    else:
+                        tablero_original[fila_aleatoria][columna_aleatoria] = emoji_aleatorio
+                        break
 
-print(tablero)
+
+llenar_tablero(5,2)
+print(tablero_original)
+print(tablero_jugador)
--- prueba.py
+++ prueba.py
@@ -1,2 +1,3 @@
-matriz = [[0 for _ in range(4)] for _ in range(4)]
-print(matriz)
+matriz = [[0,0,1],[0,2,0]]
+
+print(matriz[0][2])
