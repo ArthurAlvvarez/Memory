a05dd52282e66cbe95a5c9226bd34d08cfc3199f
ArthurAlvvarez <arturo.alvarez.gomez@colegiolitterator.com>
2024-11-06 13:57:29 +0100

    problema con la maquina, se sale del array

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from Engine import Engine
 e = Engine()
 
 #print(e.PJvsPJ(2,2,"Arthur","Emi"))
-#e.mostrar_tablero_original(5,6)
+#e.mostrar_tablero_original(2,2)
 #e.mostrar_tablero_jugador(4,5)
+#e.PJVsCPU(2,2,"Arthur","FACIL")
 e.play()
